f364f0dfefa661dc3cf563b72654ff2f3c481055
kdraxx <hiteshputta122@gmail.com>
2024-03-01 13:55:58 +0530

    created chat server using socks on 1/2/24

---
+++ Client.py
@@ -0,0 +1,27 @@
+import socket
+
+HEADER=64
+PORT=1000
+FORMAT="utf-8"
+DISCONNECT_MESSAGE="!DISCONNECT"
+SERVER="192.168.102.206"
+ADDR=(SERVER,PORT)
+
+client= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message=msg.encode(FORMAT)
+    msg_length=len(message)
+    send_length=str(msg_length).encode(FORMAT)
+    send_length+=b" "*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+while True:
+    msgg=input("Message:-")
+    send(msgg)
+    print(client.recv(64).decode(FORMAT))
+
+send(DISCONNECT_MESSAGE)
---
+++ Main_server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+HEADER=64
+HEADER2=2048
+PORT=1000
+SERVER= "192.168.102.206"
+
+s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+
+ADDR=(SERVER,PORT)
+FORMAT="utf-8"
+DISCONNECT_MESSAGE="!DISCONNECT"
+s.bind(ADDR)
+
+def handleclient(client, addr):
+    print(f"[CONNECTED] {addr} connected to the server")
+    connected=True
+    while connected:
+        msg_length=client.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length=int(msg_length)
+            msg=client.recv(msg_length).decode(FORMAT)
+            if msg==DISCONNECT_MESSAGE:
+                connected=False
+            print(f"Recieved from{addr}\nMessage:-{msg}")
+            client.send("Server has recieved message".encode(FORMAT))
+            send_msg = input("Enter the message to the client:")
+            client.send(send_msg.encode(FORMAT))
+
+    client.close()
+
+def start():
+    s.listen()
+    print(f"[LISTENING] Server is listening...{SERVER}")
+    while True:
+        client,addr=s.accept()
+        thread=threading.Thread(target=handleclient,args=(client,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+
+
+print("[STARTING],Server is starting.....")
+start()
